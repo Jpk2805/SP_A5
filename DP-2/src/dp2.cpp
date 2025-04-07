9c46b0af193884be1ac69221058f5f4f6b6ddda5
Vishw <vishweshlakhani456@gmail.com>
2025-04-07 19:52:58 -0400

    done initialize dp2 with core includes, globals, and signal handler.

--- DP-2/src/dp2.cpp
+++ DP-2/src/dp2.cpp
@@ -3,12 +3,24 @@
 #include<sys/shm.h>
 #include<stdlib.h>
 
+int semId, shmId;
+shmRegion *shm = NULL;
+
+void cleanup(int sig) {
+    if (shm) shmdt(shm);
+    exit(0);
+}
+
 int main(void){
-	//dp-1 will run dp-2 
-	//dp-2 will access the shared memory created by dp-1 
-	//by using shmid passed by dp-1
-	//
-	//then in while loop it will write the character on the circuler buffer
-	//sleep to 1/20 th of second
+
+	if (argc != 3) {
+        fprintf(stderr, "Usage: dp2 <shmId> <semId>\n");
+        return 1;
+    }
+    shmId = atoi(argv[1]);
+    semId = atoi(argv[2]);
+
+    signal(SIGINT, cleanup);
+
 	return 0;
 }
